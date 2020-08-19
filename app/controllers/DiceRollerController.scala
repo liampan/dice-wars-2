b9547d43cc824c31d399f234064a8ec0e2b2cf45
danewatts <danewatts97@gmail.com>
2020-08-19 16:17:02 +0100

    Fixed dice css

--- app/controllers/DiceRollerController.scala
+++ app/controllers/DiceRollerController.scala
@@ -2,8 +2,7 @@ package controllers
 
 import com.google.inject.Inject
 import models.Dice._
-import models.Symbol
-import models.{DiceFace, Die}
+import models.{DiceFace, Die, Symbol}
 import play.api.mvc._
 import services.DiceCancellingService
 import views.html.diceroller
@@ -15,7 +14,7 @@ class DiceRollerController @Inject()(
                                     ) extends AbstractController(cc) {
 
   def onPageLoad(): Action[AnyContent] = Action {
-    val diceToRoll = List(BlueBoost, BlueBoost, BlueBoost) // TODO these would be determined by the request
+    val diceToRoll = List(BlueBoost, GreenAbility, YellowProficiency, BlackSetback, RedChallenge, PurpleDifficulty) // TODO these would be determined by the request
     val rolledDice: RolledDice = diceToRoll.map(die => die.roll)
     val outcome = cancellingService.cancelDice(rolledDice.symbols)
     Ok(view(ViewModel(rolledDice, outcome)))
@@ -27,11 +26,10 @@ case class ViewModel(rolledDice: RolledDice, outcome: List[Symbol]){
   def outcomeScreenSize(die: Die, diceFace: DiceFace): Double =
     if(diceFace.symbols.size >= 2) (1.0/2.0) * outcomeScreenSize
     else Math.floor(die.screenSizeMultiplier * outcomeScreenSize)
-  //TODO could boost be bigger?
   //TODO css refactor
-  def imgSource(symbol: Symbol): String = {
+  def imgSource(symbol: Symbol): String =
     s"/assets/images/dice/${symbol.toLowerCase}.png"
-  }
+
   def getDiceClass(die: Die, diceFace: DiceFace): String =
     die.name + diceFace.symbols.size
 }
