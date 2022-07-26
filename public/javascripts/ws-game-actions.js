7caf4b94864f4e15b47b84110d360fbfc71d549f
Liam Oldfield <liamoldfield@hotmail.co.uk>
2022-07-26 14:34:52 +0100

    audio notify for turn

--- public/javascripts/ws-game-actions.js
+++ public/javascripts/ws-game-actions.js
@@ -10,12 +10,36 @@ function onMessage(evt) {
     if (received_msg == 'get-board') sendMessage(received_msg)
     else {
         document.getElementById('screen').innerHTML = received_msg;
+        setIcon()
     }
 }
 
 function onOpen(evt) {
     sendMessage('get-board')
     console.log('web socket open.')
+}
+
+var playNoise = true
+
+function notify() {
+    if (playNoise) {
+        var audio = document.getElementById("audio");
+        audio.play();
+    }
+}
+
+function toggleNoise() {
+    playNoise = !playNoise
+    setIcon()
+}
+
+function setIcon() {
+    let img = document.getElementById('sound');
+    if (playNoise) {
+        img.src = "/assets/images/speaker.svg"
+    } else {
+        img.src = "/assets/images/mute.svg.png"
+    }
 }
 
 // arrow might not be placed right if screen changes size
